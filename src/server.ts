14332496380448566494977a17843c7ec143cf14
devjohn42 <devjohncb@gmail.com>
2025-09-06 14:21:10 -0300

    :sparkles: feat: creates get attendee badge route

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 import { createEvent } from './http/create-event.js'
+import { getAttendeeBadge } from './http/get-attendee-badge.js'
 import { getEvent } from './http/get-event.js'
 import { registerForEvent } from './http/register-for-event.js'
 
@@ -12,6 +13,7 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(createEvent)
 app.register(registerForEvent)
 app.register(getEvent)
+app.register(getAttendeeBadge)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP Server Runnig')
